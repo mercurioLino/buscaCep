83c379e116d7e4c467065ddefe00ea4878c02dcf
mercurioLino <leonardomercurio@hotmail.com>
2021-11-16 01:00:16 -0300

    Aplicação para o Heroku

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const axios = require('axios');
-
+const port = process.env.PORT || 3000;
 app.use(cors());
+
+app.use(express.static('public'));
 
 app.get('/:cep', async(req, res) => {
     const { cep } = req.params;
@@ -15,4 +17,4 @@ app.get('/:cep', async(req, res) => {
     }
 });
 
-app.listen('4567');
+app.listen(port);
